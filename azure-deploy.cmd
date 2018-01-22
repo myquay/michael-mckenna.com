aedbc696bd14a578ef7d9fff1d5755681bce998b
Michael McKenna <michael@solve.solutions>
2018-01-22 22:45:09 +1300

    OPath

--- azure-deploy.cmd
+++ azure-deploy.cmd
@@ -1,5 +1,5 @@
 :: 2. Hugo in temporary path
-%DEPLOYMENT_SOURCE%/bin/hugo.exe -s %DEPLOYMENT_SOURCE%/blog -d %DEPLOYMENT_TARGET% --log -v
+%DEPLOYMENT_SOURCE%\bin\hugo.exe -s %DEPLOYMENT_SOURCE%\blog -d %DEPLOYMENT_TARGET% --log -v
 
 :: 3. Move the web.config to the root
 :: mv "$Env:DEPLOYMENT_SOURCE/web.config" "$Env:DEPLOYMENT_TARGET/public/web.config"
